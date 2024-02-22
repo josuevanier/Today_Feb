c59b74626f86b8d9970624fd807c0ca8aed7817d
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-02-22 14:54:39 -0500

    A glimse of the methods

--- src/main/java/org/example/ListArrays.java
+++ src/main/java/org/example/ListArrays.java
@@ -3,11 +3,17 @@ import java.util.ArrayList;
 
 public class ListArrays {
     public static void main(String[] args) {
+        // When remove item the index shrink since the size shrink
         ArrayList<Integer> integers = new ArrayList<>();
 
         integers.add(0,1);
-
-
         System.out.println(integers.get(0));
+        integers.set(0,2);
+        integers.add(3);
+
+        integers.remove(0);
+
+        System.out.println(integers.size());
+
     }
 }
