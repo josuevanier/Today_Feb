c5da72e6915ae8ae848e7098a46002fa0181d704
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-02-22 14:47:05 -0500

    ListArray

---
+++ src/main/java/org/example/ListArrays.java
@@ -0,0 +1,13 @@
+package org.example;
+import java.util.ArrayList;
+
+public class ListArrays {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+
+        integers.add(0,1);
+
+
+        System.out.println(integers.get(0));
+    }
+}
