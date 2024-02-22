5eeace5148910a2603bbfd1bb7bd58bae68ed1e2
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-02-22 15:33:19 -0500

    List is the top of hierachy so you can refer the array  List since the List

--- src/main/java/org/example/ComparableAndComparator.java
+++ src/main/java/org/example/ComparableAndComparator.java
@@ -1,4 +1,31 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class ComparableAndComparator {
+    public static void main(String[] args) {
+
+        int [] arr =  {1, 24, 4 ,5, 45 };
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        String[] names = {"asi", "Canada", "Montreal ",};
+
+        System.out.println(Arrays.toString(names));
+
+        List<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+
+        fruits.add("apple");
+
+        fruits.add("aPple");// will be printed before the apple without capilator
+
+        Collections.sort(fruits); // for a list collection sort method
+
+        System.out.println(fruits); //
+    }
 }
