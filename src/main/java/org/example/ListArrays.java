b49c1f36cf7a93ceb044ee8fdb51037d6753d002
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-02-22 15:04:14 -0500

    Enhance for loops with array list

--- src/main/java/org/example/ListArrays.java
+++ src/main/java/org/example/ListArrays.java
@@ -1,23 +1,24 @@
 package org.example;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ListArrays {
     public static void main(String[] args) {
-        // When remove item the index shrink since the size shrink
-        ArrayList<Integer> integers = new ArrayList<>();
 
-        integers.add(0,1);
-        System.out.println(integers.get(0));
-        integers.set(0,2);
-        integers.add(3);
+        // Integer
 
-        integers.remove(0);
+        ArrayList<Integer> integers = new ArrayList<>(); // reference point a
 
-        System.out.println(integers.size());
+        integers.add(1);
 
-        for(int i = 0; i < integers.size();i++){
-            System.out.println(integers.get(i) + " ");
-        }
+        integers.add(-1);
 
+        integers.add(2);
+
+        integers.remove(Integer.valueOf(1)); // REMOVE BY THE VALUE SINCE ITS A WRAPPER CLASS
+
+        Collections.sort(integers); // need to import the package sort the package ddfd
+
+        for(int intValues : integers) System.out.println( intValues + ", ");
     }
 }
