cfd3027320745458b7824978af864906a4e4ddf0
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-02-22 15:50:39 -0500

    compareTo() -how we add a set of object into an array list pay attention to the comments
    - Collection.sort(authors) sorint arraylist.
    
    - Sorting will be overiden by  sorint compare too
    - Author a : authors enhance for loops with objects !

--- src/main/java/org/example/ComparableAndComparator.java
+++ src/main/java/org/example/ComparableAndComparator.java
@@ -8,24 +8,35 @@ import java.util.List;
 public class ComparableAndComparator {
     public static void main(String[] args) {
 
-        int [] arr =  {1, 24, 4 ,5, 45 };
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        // the reason we hacve author in the second part is because if author have child class so it can be upcasted
+        ArrayList<Author> authors = new ArrayList<Author>();
+        authors.add(new Author("Elon","Musk","Space"));
+        authors.add(new Author("Bill","Gates","Ms"));
+        authors.add(new Author("Elon","Musk","Apple"));
+        authors.add(new Author("Elon","Musk","Space"));
 
-        String[] names = {"asi", "Canada", "Montreal ",};
+        Collections.sort(authors);
 
-        System.out.println(Arrays.toString(names));
+        System.out.println("The sorted author by last");
 
-        List<String> fruits = new ArrayList<>();
+        // enhanced for loop
+        for(Author a : authors){
+            System.out.println(a.firstName + " " + a.lastName + "[ Book' name : " + a.bookName + "]");
+        }
+    }
+}
+class Author implements Comparable<Author>{
+    String firstName;
+    String lastName;
+    String bookName;
 
-        fruits.add("Apple");
-
-        fruits.add("apple");
-
-        fruits.add("aPple");// will be printed before the apple without capilator
-
-        Collections.sort(fruits); // for a list collection sort method
-
-        System.out.println(fruits); //
+    public Author(String firstName, String lastName, String bookName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.bookName = bookName;
+    }
+    @Override
+    public int compareTo(Author o){
+        return this.lastName.compareTo(o.lastName); // based on the  value we will sort
     }
 }
