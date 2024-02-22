09a35c3aa1989f37ba9a713f145c1d2c1dc8c2da
josuevanier <150824779+josuevanier@users.noreply.github.com>
2024-02-22 16:16:32 -0500

    Sample logic the reason we had to do the logic different as a String is because the string has its own compare method but not integer so we had to impliment a real logic

--- src/main/java/org/example/ComparableAndComparator.java
+++ src/main/java/org/example/ComparableAndComparator.java
@@ -8,35 +8,71 @@ import java.util.List;
 public class ComparableAndComparator {
     public static void main(String[] args) {
 
-        // the reason we hacve author in the second part is because if author have child class so it can be upcasted
-        ArrayList<Author> authors = new ArrayList<Author>();
-        authors.add(new Author("Elon","Musk","Space"));
-        authors.add(new Author("Bill","Gates","Ms"));
-        authors.add(new Author("Elon","Musk","Apple"));
-        authors.add(new Author("Elon","Musk","Space"));
+        ArrayList<Laptop> laptops = new ArrayList<Laptop>();
 
-        Collections.sort(authors);
+        laptops.add(new Laptop("Mac", 2,3));
+        laptops.add(new Laptop("Hd", 3,2));
+        laptops.add(new Laptop("Mac", 4,1));
 
-        System.out.println("The sorted author by last");
+        Collections.sort(laptops);
 
-        // enhanced for loop
-        for(Author a : authors){
-            System.out.println(a.firstName + " " + a.lastName + "[ Book' name : " + a.bookName + "]");
-        }
+        for(Laptop laptop : laptops) System.out.println(laptop.name + " " + laptop.price +
+                " " + laptop.ram);
+
     }
 }
-class Author implements Comparable<Author>{
-    String firstName;
-    String lastName;
-    String bookName;
+class Laptop implements Comparable<Laptop>{
+    String name;
+    int ram;
 
-    public Author(String firstName, String lastName, String bookName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.bookName = bookName;
+    int price;
+
+    public Laptop(String name, int ram, int price) {
+        this.name = name;
+        this.ram = ram;
+        this.price = price;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     @Override
-    public int compareTo(Author o){
-        return this.lastName.compareTo(o.lastName); // based on the  value we will sort
+    public String toString() {
+        return "Laptop{" +
+                "name='" + name + '\'' +
+                ", ram=" + ram +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Laptop o) {
+        // Sort based on the access random Memory
+        if(this.ram == o.getRam()) return 0;
+        else if (this.ram >  o.getRam()) {
+            return 1;
+        }
+        else return  -1;
     }
 }
